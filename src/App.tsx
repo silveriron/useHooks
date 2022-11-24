f97cc171144913f09f49f61913df402f06b92343
kichul7493 <kichul7493@gmail.com>
2022-11-24 11:18:42 +0900

    add useInput

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import useInput from "./hooks/useInput"
 
-function App() {
+const App = () => {
+  const {value: name, onChange: nameHandler } = useInput('Guest')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" value={name} onChange={nameHandler} />
+      <button onClick={() => console.log(name)} >Print</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
